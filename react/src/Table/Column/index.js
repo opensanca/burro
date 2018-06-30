0533d7d450317033ba7b4601bb16732975a3da38
Guilherme Diego <guidiego.expgames@gmail.com>
2018-06-29 22:39:33 -0300

    Add: Column Element

--- react/src/Table/Column/index.js
+++ react/src/Table/Column/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Column = ({ header, children }) => header ? (
+export const Column = ({ header, children }) => (header ? (
   <th>{ children }</th>
 ) : (
   <td>{ children }</td>
-);
+));
 
 Column.propTypes = {
   header: PropTypes.bool,
@@ -13,7 +13,7 @@ Column.propTypes = {
 };
 
 Column.defaultProps = {
-  header: false
+  header: false,
 };
 
 export default Column;
