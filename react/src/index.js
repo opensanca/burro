78212541be600f69de6e7c3475393bcd056378a8
Guilherme Diego <guidiego.expgames@gmail.com>
2018-07-14 23:16:34 -0300

    Add: Deploy Stuff

---
+++ react/src/index.js
@@ -0,0 +1,7 @@
+export * from './Tooltip';
+export * from './Button';
+export * from './Dropdown';
+export * from './Input';
+export * from './Table';
+export * from './Header';
+export * from './Grid';
